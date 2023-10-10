5685da0a6af7f6034903148d5ae41bfaa217d518
Ailton Xavier <ailton.xavier@hotmail.com>
2023-10-10 16:22:20 -0300

    muito bom

--- main.py
+++ main.py
@@ -1,14 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+import time
 
-response = requests.get("https://g1.globo.com")
 
-content = response.content
+i = 0
+while i != 1:
+    response = requests.get("https://g1.globo.com")
 
-site = BeautifulSoup(content, "html.parser")
+    content = response.content
 
-noticias = site.find_all('div', attrs={'class':'feed-post-body'})
+    site = BeautifulSoup(content, "html.parser")
 
-for noticia in noticias:
-    titulo = noticia.find('a', attrs={'class':'feed-post-link'})
-    print(titulo.text)
+    noticias = site.find_all('div', attrs={'class':'feed-post-body'})
+    links = []
+    conteudo_bd = []
+    for noticia in noticias:
+        titulo = (noticia.find('a', attrs={'class':'feed-post-link'}))
+        links += [titulo.get('href')]
+        
+    for link in links:
+        request_lista = requests.get(link).text
+        titulo_sub_link = (request_lista.find('a', attrs={'class':'feed-post-link'}))
+        
+        conteudo_bd.append(request_lista)
+
+    print(conteudo_bd)
+    time.sleep(5)
