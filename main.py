896269da9d55e757f1d6b231ef322e19d9c18ce7
Ailton Xavier <ailton.xavier@hotmail.com>
2023-10-10 15:50:39 -0300

    web scraping G1

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
+
+response = requests.get("https://g1.globo.com")
+
+content = response.content
+
+site = BeautifulSoup(content, "html.parser")
+
+noticias = site.find_all('div', attrs={'class':'feed-post-body'})
+
+for noticia in noticias:
+    titulo = noticia.find('a', attrs={'class':'feed-post-link'})
+    print(titulo.text)
