9cfa6e38d95af2182165398aed0293813eed30a8
Ailton Xavier <ailton.xavier@hotmail.com>
2023-10-16 17:33:41 -0300

    inserindo data e hora

--- main.py
+++ main.py
@@ -1,25 +1,38 @@
 import requests
 from bs4 import BeautifulSoup
 from time import sleep
+from datetime import datetime
 
 while 1 != 2:
+    data_atual = datetime.now().strftime("%H:%M:%S")
+    hora_atual = datetime.today().strftime("%d/%m/%y")
     request = requests.get("https://g1.globo.com").content
     soup = BeautifulSoup(request, 'html.parser')
     links = []
     titulos = []
+    data_base = []
 
     for link in soup.find_all('a', attrs={'class':'feed-post-link'}):
-        # adicionando o link das paginas na lista de links
-        if link.get('href') != None: links.append(link.get('href'))
+        # adicionando o link das paginas na lista de links e evitando exceção em caso de retorno None do link
+        if link.get('href') != None: 
+            links.append(link.get('href'))
 
     for link in links:
         request2 = requests.get(links[links.index(link)]).content
         soup2 = BeautifulSoup(request2, 'html.parser')
         titulo = soup2.find('h1', attrs={'class':'content-head__title'})
         if titulo != None: 
-            print(titulo.text)
             titulos.append(titulo.text)
     
-    print(titulos)
+    lista = [titulos, data_atual, hora_atual]
+    data_base.append(lista)
     print()
+    print(data_base)
     sleep(3)
+
+"""
+1 - autenticar
+2 - popular o banco
+2.1 (Título das notícias, data, hora)
+3 - plotar em graph
+"""
