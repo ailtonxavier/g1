df8e0164f259ef6a71b9b14d7eeea48106b41070
Ailton Xavier <ailton.xavier@hotmail.com>
2023-10-18 17:12:21 -0300

    inserindo banco de dados

--- main.py
+++ main.py
@@ -1,38 +1,72 @@
 import requests
 from bs4 import BeautifulSoup
-from time import sleep
 from datetime import datetime
+from time import sleep
+import sqlite3
 
-while 1 != 2:
-    data_atual = datetime.now().strftime("%H:%M:%S")
-    hora_atual = datetime.today().strftime("%d/%m/%y")
-    request = requests.get("https://g1.globo.com").content
-    soup = BeautifulSoup(request, 'html.parser')
+
+# conexao database
+banco = 'database.db'
+conexao = sqlite3.connect(banco)
+connection = conexao.cursor()
+connection.execute('CREATE TABLE IF NOT EXISTS noticias (noticia TEXT, hora TEXT, data TEXT);')
+conexao.close()
+
+
+def autenticacao():
+    pass
+
+def g1():
+    return BeautifulSoup(requests.get("https://g1.globo.com").content, 'html.parser')
+
+def encontra_links(g1):
     links = []
-    titulos = []
-    data_base = []
-
-    for link in soup.find_all('a', attrs={'class':'feed-post-link'}):
-        # adicionando o link das paginas na lista de links e evitando exceção em caso de retorno None do link
+    for link in g1.find_all('a', attrs={'class':'feed-post-link'}):
         if link.get('href') != None: 
             links.append(link.get('href'))
+    return links
 
+def listagem_de_titulos(links):
+    titulos = []
     for link in links:
-        request2 = requests.get(links[links.index(link)]).content
-        soup2 = BeautifulSoup(request2, 'html.parser')
-        titulo = soup2.find('h1', attrs={'class':'content-head__title'})
+        titulo = (BeautifulSoup(requests.get(links[links.index(link)]).content, 'html.parser')).find('h1', attrs={'class':'content-head__title'})
         if titulo != None: 
             titulos.append(titulo.text)
-    
-    lista = [titulos, data_atual, hora_atual]
-    data_base.append(lista)
-    print()
-    print(data_base)
-    sleep(3)
+    return titulos
 
-"""
-1 - autenticar
-2 - popular o banco
-2.1 (Título das notícias, data, hora)
-3 - plotar em graph
-"""
+def data_e_hora():# cursor
+    return [datetime.now().strftime("%H:%M:%S"), datetime.today().strftime("%d/%m/%y")]
+
+while 1 != 2:
+
+    links = encontra_links(g1())
+
+    titulos = listagem_de_titulos(links)
+
+    data_e_hora_atual = data_e_hora()
+
+    try:
+        links = encontra_links(g1())
+
+        titulos = listagem_de_titulos(links)
+
+        data_e_hora_atual = data_e_hora()
+
+        titulo = ", ".join(titulos)
+
+        # banco
+        conexao = sqlite3.connect(banco)
+        connection = conexao.cursor()
+        connection = (sqlite3.connect(banco)).cursor()
+        connection.execute(f'insert into noticias values("{titulo}","{data_e_hora_atual[0]}","{data_e_hora_atual[1]}")')
+        sqlite3.connect(banco).commit()
+        connection.execute("select * from noticias")
+        print(connection.fetchall())
+        conexao.close()
+        
+    except sqlite3.Error as error:
+        print(error)
+
+
+    
+    sleep(3)
